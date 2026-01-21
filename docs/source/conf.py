02d15ee538ddcf049545727c4879a91326206264
legole0 <legole0@proton.me>
2026-01-21 06:56:41 -0300

    Config file basic setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Rubicon Engine'
+copyright = '2024-Present, Rubicon Team'
+author = 'Rubicon Team'
 
 release = '0.1'
 version = '0.1.0'
